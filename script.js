392a6ba15d7d4d9eb37188bf8dce4f7ae9e1b62c
GUSTAVO DE SOUZA BRAGA <gustavo_s_braga@estudante.sesisenai.org.br>
2023-08-02 12:34:14 -0300

    finishied

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 async function traduzir() {
+  const inputLanguage = document.getElementById("inputLanguage");
+  const outputLanguage = document.getElementById("outputLanguage");
+  const textToTranslate = document.getElementById("textToTranslate");
+  const showResult = document.getElementById("result");
+
+  if (!textToTranslate.value) return alert("Insira um texto para traduzir");
+  if (inputLanguage.value == outputLanguage.value) return alert("Escolha uma linguagem de entrada diferente da de saída");
+
   const url = "https://google-translate1.p.rapidapi.com/language/translate/v2";
   const options = {
     method: "POST",
@@ -9,17 +17,33 @@ async function traduzir() {
       "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
     },
     body: new URLSearchParams({
-      q: "Hello, world!",
-      target: "es",
-      source: "en",
+      q: textToTranslate.value,
+      target: outputLanguage.value,
+      source: inputLanguage.value,
     }),
   };
 
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result);
+    console.log(result.data.translations[0].translatedText);
+    showResult.innerText = result.data.translations[0].translatedText;
   } catch (error) {
     console.error(error);
   }
+}
+
+function switchLanguage() {
+  const inputLanguage = document.getElementById("inputLanguage");
+  const outputLanguage = document.getElementById("outputLanguage");
+  const textToTranslate = document.getElementById("textToTranslate");
+  const showResult = document.getElementById("result");
+  let initInputValue = inputLanguage.value;
+  let initOutputValue = outputLanguage.value;
+  let textoToTranslate = textToTranslate.value;
+  let showtoResult = showResult.value;
+  inputLanguage.value = initOutputValue;
+  outputLanguage.value = initInputValue;
+  textToTranslate.value = showtoResult;
+  showResult.value = textoToTranslate;
 }
